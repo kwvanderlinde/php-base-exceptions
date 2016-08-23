b15f0611ae92105e7ddea9b22a9084540d31d0d8
Kenneth VanderLinde <kwvanderlinde@hotmail.com>
2016-08-22 21:30:44 -0700

    Documented exception classes

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -1,7 +1,24 @@
 <?php
 namespace Base\Exceptions;
 
+/**
+ * The root of the contingency and fault hierarchy.
+ *
+ * This isn't really the root, because it necessarily extends `\Exception`.
+ * However, it is provided because we would like some independence from the
+ * standard exception root. For instance, we provide a constructor which does
+ * not accept exception codes.
+ */
 abstract class Exception extends \Exception {
+	/**
+	 * Initializes an `Exception`.
+	 *
+	 * @param string $message
+	 * A human-readable message to accompany the exception.
+	 *
+	 * @param \Exception|null $previous
+	 * A previous exception which caused this exception.
+	 */
 	public function __construct($message = '', \Exception $previous = null) {
 		parent::__construct($message, 0, $previous);
 	}
